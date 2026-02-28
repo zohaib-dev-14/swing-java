77a3a242a6ae0acdac964b9efd203e7f0c3254c3
Muhammad Zohaib <zohaibsaraj10@gmail.com>
2026-03-01 00:51:09 +0500

    AWT setting frame

---
+++ src/AWT_BASIC_1.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+public class AWT_BASIC_1 {
+
+	public static void main(String[] args) {
+		
+		Frame frame = new Frame("My First Program");
+		frame.setSize(400, 200);
+		frame.setVisible(true);
+
+	}
+
+}
