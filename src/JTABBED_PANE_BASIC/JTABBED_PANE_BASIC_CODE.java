e9e77d6723096825040cd6720187ccfa503cc396
Muhammad Zohaib <zohaibsaraj10@gmail.com>
2026-03-01 19:09:20 +0500

    all files added

--- src/JTABBED_PANE_BASIC/JTABBED_PANE_BASIC_CODE.java
+++ src/JTABBED_PANE_BASIC/JTABBED_PANE_BASIC_CODE.java
@@ -11,23 +11,22 @@ public class JTABBED_PANE_BASIC_CODE {
 		
 		JTabbedPane tabbedPane = new JTabbedPane();
 		
+		
 		JPanel addPanel = new JPanel();
-		addPanel.add(new JLabel("Add Records Here", JLabel.CENTER));
-		
-		JPanel displayPanel = new JPanel();
-		displayPanel.add(new JLabel("Display Records Here", JLabel.CENTER));
-		
+		addPanel.add(new JLabel("Add records here"));
 		JPanel updatePanel = new JPanel();
-		updatePanel.add(new JLabel("Update Records Here", JLabel.CENTER));
+		updatePanel.add(new JLabel("update records here"));
+		JPanel describePanel = new JPanel();
+		describePanel.add(new JLabel("describe records here"));
 		
 		tabbedPane.add("Add", addPanel);
-		tabbedPane.add("Display", displayPanel);
+		tabbedPane.add("Describe", describePanel);
 		tabbedPane.add("Update", updatePanel);
 		
 		
         frame.add(tabbedPane);
 
-        frame.setSize(400,400);
+        frame.setSize(400,300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
